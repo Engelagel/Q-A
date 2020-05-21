1fe2d4222b52e4d1b64cbaeb66de35dda783f9ad
natEngelking <natalie.mixdorf@gmail.com>
2020-05-21 12:06:28 -0600

    swerved the auto increment

--- SDC/database/db.js
+++ SDC/database/db.js
@@ -5,47 +5,35 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 
-let questionSchema = new mongoose.Schema({
+let qaSchema = new mongoose.Schema({
+  //secondary index
     product_id: Number,
     results: [{
-          question_id: Number,
-          question_body: String,
-          question_date: Date,
-          asker_name: String,
-          question_helpfulness: Number,
-          reported: Number,
-          answers: {
-            autoIncreNum: {
-              id: Number,
-              body: String,
-              date: Date,
-              answerer_name: String,
-              helpfulness: Number,
-              photos: [{
-                id: Number,
-                url: String
-              }]
-            }
-          }
-        }
-    ]
-});
-
-let answerSchema = new mongoose.Schema({
-    question: Number,
-    page: Number,
-    count: Number,
-    results: [
-      {
-        answer_id: Number,
-        body: String,
-        date: Date,
-        answerer_name: String,
-        helpfulness: Number,
-        photos: [{
+      question_id: Number,
+      question_body: String,
+      question_date: Date,
+      asker_name: String,
+      question_helpfulness: Number,
+      reported: Number,
+      answers: {
+        question: Number,
+        page: Number,
+        count: Number,
+        results: [{
+          answer_id: Number,
+          body: String,
+          date: Date,
+          answerer_name: String,
+          helpfulness: Number,
+          photos: [{
             id: Number,
             url: String
           }]
+        }]
       }
-    ]
-})
+    }]
+});
+
+const qa = mongoose.model('qa', qaSchema)
+
+module.exports = qa;
