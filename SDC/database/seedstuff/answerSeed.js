936670f9c2182e4fa3438e7309218803ad850ef5
natEngelking <natalie.mixdorf@gmail.com>
2020-05-27 09:38:48 -0600

    data seeded

--- SDC/database/seedstuff/answerSeed.js
+++ SDC/database/seedstuff/answerSeed.js
@@ -1,33 +1,5 @@
-const faker = require('faker');
-const { PerformanceObserver, performance} = require('perf_hooks');
+//import scripts for answers
 
-let answers = [];
-
-const generateAnswers = () => {
-  let M = 100000
-  let n = 0;
-  while (n < M) {
-    answers.push({
-      question_id: faker.random.number((M * 2)),
-      answer_id: faker.random.number((M * 2)),
-      body: faker.random.words(35),
-      date: faker.date.recent(),
-      answerer_name: faker.internet.userName(),
-      helpfulness: faker.random.number(20),
-      reported: faker.random.number(20),
-      photos: [faker.random.number((M * 2))],
-    })
-    n++;
-  }
-}
-
-
-const wrappedA = performance.timerify(generateAnswers);
-
-const obs = new PerformanceObserver((list) => {
-  console.log(list.getEntries()[0].duration);
-  obs.disconnect();
-});
-obs.observe({ entryTypes: ['function'] });
-
-wrapped();
+//mongoimport --db=sdc --collection=answer --type=json  --file='C:\GitHub\Q-A\SDC\database\seedstuff\adata1.json'
+//mongoimport --db=sdc --collection=answer --type=json  --file='C:\GitHub\Q-A\SDC\database\seedstuff\adata2.json'
+//mongoimport --db=sdc --collection=answer --type=json  --file='C:\GitHub\Q-A\SDC\database\seedstuff\adata3.json'
--- SDC/database/seedstuff/photoSeed.js
+++ SDC/database/seedstuff/photoSeed.js
@@ -1,27 +1,3 @@
-const faker = require('faker');
-const { PerformanceObserver, performance} = require('perf_hooks');
 
-
-let photos = [];
-
-const generatePhotos = () => {
-  let M = 100000
-  let n = 0;
-  while (n < M) {
-    photos.push({
-      id: faker.random.number((M * 2)),
-      url: faker.random.word(),
-    })
-    n++;
-  }
-}
-
-const wrapped = performance.timerify(generatePhotos);
-
-const obs = new PerformanceObserver((list) => {
-  console.log(list.getEntries()[0].duration);
-  obs.disconnect();
-});
-obs.observe({ entryTypes: ['function'] });
-
-wrapped();
+//import scripts for photo
+// mongoimport --db=sdc --collection=photo --type=json  --file='C:\GitHub\Q-A\SDC\database\seedstuff\pdata3.json'
