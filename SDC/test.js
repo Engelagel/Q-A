4136a0b289fead6db831c021b260883024ddeaec
natEngelking <natalie.mixdorf@gmail.com>
2020-05-30 13:01:38 -0600

    ec2 stuff

--- SDC/test.js
+++ SDC/test.js
@@ -1,7 +1,12 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
+import { Rate } from 'k6/metrics';
+
+export const errorRate = new Rate('errors');
 
 export let options = {
+  vus: 1,
+  duration: '5s',
   stages: [
     {duration: "30s", target: 20},
     {duration: "1m30s", target: 10},
@@ -19,8 +24,9 @@ const localPutAReport = "http://localhost:3004/qa/answer/7/report"
 
 export default () => {
   let res = http.get(localGetQuestion);
-  check(res, {
-    "status was 200": (r) => r.status == 200
+  const result = check(res, {
+    "is status 200": (r) => r.status == 200
   });
+  errorRate.add(!result);
   sleep(1)
 }
